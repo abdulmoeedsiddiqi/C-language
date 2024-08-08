8cf6d3dbf3f2b43bd3adf444efbb86be08d8acd5
abdulmoeedsiddiqi <u2023009@giki.edu.pk>
2024-08-08 16:47:56 +0500

    _

--- prime_number.c
+++ prime_number.c
@@ -1,11 +1,13 @@
 #include<stdio.h>
+#include<math.h>
 int main(){
     int n, check=0;
     printf("Enter a number : ");
     scanf("%d",&n);
-    for(int i=2; i<n ; i++){
+    for(int i=2; i<=sqrt(n) ; i++){
         if(n%i==0){
             check=1;
+            break;
         }
     }
 
